474ed2f326d48eb5ad926865c2e79edac4f61f24
SunmoonL <dltjsans123@naver.com>
2022-12-01 15:50:47 +0900

    sub select jquery

--- js/sub.js
+++ js/sub.js
@@ -42,3 +42,26 @@ $(function(){
          $('.list').hide();
     });
 });
+
+$(document).on('change',"#headset",function(){
+
+    if($(this).val() == '제품을 선택하세요'){
+        $('.totalprice').text('제품을 선택하세요.');
+        $('#one').text('(총0개)');
+    }
+
+    if($(this).val() == 'VOID RGB ELITE WHITE'){
+        $('.totalprice').text('159,000원');
+        $('#one').text('(총1개)');
+    }
+
+    if($(this).val() == 'VOID RGB ELITE BLACK'){
+        $('.totalprice').text('일시 품절입니다.');
+        $('#one').text('(총0개)');
+    }
+
+    if($(this).val() == 'VOID RGB ELITE CHERRY'){
+        $('.totalprice').text('159,000원');
+        $('#one').text('(총1개)');
+    }
+});
